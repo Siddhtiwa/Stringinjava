27017ca2fef7214a527355c6609ff13e313b3258
siddhant tiwari <153662147+Siddhtiwa@users.noreply.github.com>
2024-06-08 17:40:20 +0530

    basic string concept

---
+++ contains.java
@@ -0,0 +1,11 @@
+
+
+public class contains {
+    public static void main(String[] args) {
+        String s= "siddhant tiwari";
+        System.out.println(s.contains("ddhant"));
+        System.out.println(s.contains("krish"));
+    }
+
+
+}
---
+++ indexof.java
@@ -0,0 +1,9 @@
+public class indexof {
+    public static void main(String[] args) {
+        String s = " siddhant tiwari ";
+        System.out.println(s.indexOf('d'));
+        System.out.println(s.lastIndexOf('a'));
+        System.out.println(s.trim());
+        System.out.println(s);
+    }
+}
